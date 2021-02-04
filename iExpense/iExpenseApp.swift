9529d6d5b3b8715ac33ed580a779f558afe96a67
clearlynow <59779189+clearlynow@users.noreply.github.com>
2021-02-04 12:54:25 -0700

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Alison Gorman on 2/4/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
